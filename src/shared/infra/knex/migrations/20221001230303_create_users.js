f12d4b6801832932d7e3d7d8bd2b447c72f1586b
João V <silva.joao1@outlook.com.br>
2022-10-23 17:24:24 -0300

    feat: user migration

---
+++ src/shared/infra/knex/migrations/20221001230303_create_users.js
@@ -0,0 +1,24 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable('users', function (table) {
+    table.uuid('id').defaultTo(knex.raw('gen_random_uuid ()')).primary();
+    table.string('name', 255).notNullable();
+    table.string('email', 255).notNullable().unique();
+    table.string('password', 255).notNullable();
+    table.string('googleId', 255);
+    table.string('resetPasswordToken', 255);
+    table.dateTime('resetPasswordExpires');
+    table.timestamps(true, true, true);
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists('users');
+};
